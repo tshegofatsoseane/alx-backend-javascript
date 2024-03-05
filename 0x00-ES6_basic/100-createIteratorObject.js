edc6e2fca80b1ef0db14bd7e62800a8f73c44445
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-03-05 04:32:16 +0000

    fix: Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,20 +1,6 @@
-export default function createIteratorObject(report) {
-  const allEmployees = Object.values(report.allEmployees).flat();
-
-  const iterator = {
-    index: 0,
-    next() {
-      if (this.index < allEmployees.length) {
-        return { value: allEmployees[this.index++], done: false };
-      } else {
-        return { done: true };
-      }
-    }
-  };
-
-  iterator[Symbol.iterator] = function () {
-    return this;
-  };
-
-  return iterator;
+export default function* createIteratorObject(report) {
+  const departments = Object.values(report.allEmployees);
+  for (const employees of departments) {
+    yield* employees;
+  }
 }
