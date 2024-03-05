4c2d323ae8161e84d14bef72346d2f547e25fca8
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-03-05 03:36:01 +0000

    fix arrow functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,9 @@
-const getNeighborhoodsList = () => {
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
-};
+}
 
-export default getNeighborhoodsList;
