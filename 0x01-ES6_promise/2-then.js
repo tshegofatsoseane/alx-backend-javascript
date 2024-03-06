15504ab3fd6dda3920cf092d17235ff5adfba8e6
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-03-06 23:26:53 +0000

    refactor: handle response from API

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,6 @@
-function handleResponseFromAPI(promise) {
-  promise.then(
-    () => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    },
-    () => {
-      console.log('Got a response from the API');
-      return new Error();
-    },
-  );
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
